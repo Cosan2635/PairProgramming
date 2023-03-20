a8ba4612d3c6f6b4d5abe53acc9fbca49fde5e96
Haydar02 <hayd0281@edu.easj.dk>
2023-03-20 14:43:35 +0100

    Repository GetAll

--- PairProgramming/Repositories/MusicRepository.cs
+++ PairProgramming/Repositories/MusicRepository.cs
@@ -1,6 +1,27 @@
-﻿namespace PairProgramming.Repositories
+﻿using PairProgramming.Models;
+using System.Security.Cryptography.X509Certificates;
+
+namespace PairProgramming.Repositories
 {
     public class MusicRepository
     {
+        private List<Music> _musics;
+        private int _NextId;
+        public MusicRepository()
+        {
+            _NextId = 1;
+            _musics = new List<Music>()
+            {
+                new Music() { id = 1, title = "moo", artist = "Mohammed", duration = 150, publicationYear = 2022 },
+                new Music() { id = 2,title = "hay", artist = "Haydar",duration = 145, publicationYear = 2020}
+
+
+            };           
+        }
+        public List<Music> GetAll()
+        {
+            List<Music> list = new List<Music>();
+            return list;
+        }
     }
 }
