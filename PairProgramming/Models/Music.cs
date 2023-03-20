6f85a807336cc0654485e8878cf28d0fee30d6b6
Muhammed Cosan <muha0653@edu.easj.dk>
2023-03-20 14:48:31 +0100

    asaa

--- PairProgramming/Models/Music.cs
+++ PairProgramming/Models/Music.cs
@@ -2,5 +2,11 @@
 {
     public class Music
     {
+        public int Id { get; set; }
+        public string? title { get; set; }
+        public string? artist { get; set;}
+        public int duration { get; set; }
+        public int publicationYear { get; set; }
     }
+
 }
